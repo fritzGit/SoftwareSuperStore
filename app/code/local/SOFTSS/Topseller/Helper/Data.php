3a5d1ec23447aebc4112a73be0d033049d9654da
Juan Galvez <j.galvez@pcfritz.de>
2013-02-25 15:21:06 +0100

    styling and new catalog module with new product attributes

--- app/code/local/SOFTSS/Topseller/Helper/Data.php
+++ app/code/local/SOFTSS/Topseller/Helper/Data.php
@@ -1,6 +1,20 @@
 <?php
+/*
+ *  Software property of Pcfritz.de. Copyright 2013.
+ */
+
+/**
+ * Data.php (UTF-8)
+ *
+ * Feb 25, 2013
+ * @author Juan Galvez :: juanjogalvez@gmail.com
+ * @package SOFTSS
+ * @subpackage
+ *
+ *
+ * */
 
 class SOFTSS_Topseller_Helper_Data extends Mage_Core_Helper_Abstract
 {
-    
+
 }
