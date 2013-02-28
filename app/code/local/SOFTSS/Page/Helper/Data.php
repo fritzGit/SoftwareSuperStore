80e978791ad83a30db4b7fc06a02ee68492318d1
Juan Galvez <j.galvez@pcfritz.de>
2013-02-28 17:03:58 +0100

    footer and header images html and styling

--- app/code/local/SOFTSS/Page/Helper/Data.php
+++ app/code/local/SOFTSS/Page/Helper/Data.php
@@ -7,16 +7,15 @@
 /**
  * Data.php (UTF-8)
  *
- * Feb 25, 2013
+ * Feb 27, 2013
  * @author Juan Galvez :: juanjogalvez@gmail.com
  * @package SOFTSS
  * @subpackage
  *
  *
  * */
-class SOFTSS_Catalog_Helper_Data  extends Mage_Core_Helper_Abstract
+class SOFTSS_Page_Helper_Data extends Mage_Core_Helper_Abstract
 {
-
 }
 
 ?>
--- app/code/local/SOFTSS/TeaserEditor/Block/Teaser.php
+++ app/code/local/SOFTSS/TeaserEditor/Block/Teaser.php
@@ -1,22 +1,33 @@
 <?php
-class Softss_TeaserEditor_Block_Teaser extends Mage_Core_Block_Template {
+class Softss_TeaserEditor_Block_Teaser extends Mage_Core_Block_Template
+{
 
-	/**
-	 * Main Slider Teasers
-	 */
-	protected $_aMainSliderTeasers;
+    /**
+     * Main Slider Teasers
+     */
+    protected $_aMainSliderTeasers;
 
-        /**
-	 * Retrieve list of homepage main slider teasers
-	 *
-	 * @return $_aMainSliderTeasers
-	 */
-	public function getHomepageSliderTeasers()
-        {
-            if (is_null($this->_aMainSliderTeasers)) {
-                $this->_aMainSliderTeasers = Mage::getModel('softssteasereditor/homepage')->getSliderTeaserCollection();
-            }
+    /**
+     * Retrieve list of homepage main slider teasers
+     *
+     * @return $_aMainSliderTeasers
+     */
+    public function getHomepageSliderTeasers()
+    {
+        if (is_null($this->_aMainSliderTeasers)) {
+            $this->_aMainSliderTeasers = Mage::getModel('softssteasereditor/homepage')->getSliderTeaserCollection();
+        }
 
-            return $this->_aMainSliderTeasers;
-	}
+        return $this->_aMainSliderTeasers;
+    }
+
+     /**
+     * Check if current url is url for home page
+     *
+     * @return true
+     */
+    public function getIsHomePage()
+    {
+        return $this->getUrl('') == $this->getUrl('*/*/*', array('_current'=>true, '_use_rewrite'=>true));
+    }
 }
