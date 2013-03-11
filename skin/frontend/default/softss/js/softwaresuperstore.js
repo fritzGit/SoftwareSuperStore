bb93853da03c7d071e0c7ca60968451479cee48c
Entwickler 3 <n.entwickler@pcfritz.de>
2013-03-11 17:15:18 +0100

    ajax login

--- skin/frontend/default/softss/js/softwaresuperstore.js
+++ skin/frontend/default/softss/js/softwaresuperstore.js
@@ -60,4 +60,54 @@ function sortbyChange(selectObj){
         setLocation(selectObj.value);
 }
 
+function showAjaxLoginForm(){
+    
+       var overlay=$j('#overlaylogin');
+       overlay.fadeIn();
+       var ajaxContainer = $j('#ajax_login_container');
+       ajaxContainer.css("top", ( $j(window).height() - ajaxContainer.height() ) / 2+$j(window).scrollTop() + "px");
+       ajaxContainer.css("left", ( $j(window).width() - ajaxContainer.width() ) / 2+$j(window).scrollLeft() + "px");
+       ajaxContainer.fadeIn();
+}
+
+function ajaxLoginPost(){
+    
+    var overlay=$j('#overlaylogin');
+    var ajaxurl=baseurl+'customer/account/loginAjaxPost';
+    var loginForm = new VarienForm('login-form-ajax', true);
+
+    if(loginForm.validator && loginForm.validator.validate()){ 
+        $j.ajax({
+            url: ajaxurl,
+            type: 'POST',
+            data: $j('#login-form-ajax').serialize(),
+            dataType: 'json',
+            beforeSend: function() {
+                var imageurl = baseurl+'skin/frontend/default/softss/images/ajax-loader.gif';
+                overlay.html('<img id="imgloading" src="'+imageurl+'" />').show();
+            },
+            success: function(response) {
+                overlay.html('');  
+                if(response.error) {             
+                     var message = '<ul class="messages"><li class="error-msg"><ul><li>' + response.message + '</li></ul></li></ul>';
+                     $j('#message-div').html(message);
+                } else {
+                   window.location.href=response.url;
+                } 
+                var ajaxContainer = $j('#ajax_login_container');
+                ajaxContainer.fadeIn();
+            },
+            error: function(response) {
+                overlay.hide();            
+                alert("Error while trying to login.");
+            }
+        });
+    }
+}
+
+function hideLoginOverlay(){
+    $j('#overlaylogin').fadeOut();
+    $j('#ajax_login_container').fadeOut();
+}
+
 //]]>
