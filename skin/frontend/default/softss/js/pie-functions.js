c91cc110e1d45043e140adb3b6931fc5e6c570c2
Juan Galvez <j.galvez@pcfritz.de>
2013-04-23 17:24:31 +0200

    styling

--- skin/frontend/default/softss/js/pie-functions.js
+++ skin/frontend/default/softss/js/pie-functions.js
@@ -1,7 +1,7 @@
 (function ($) {
     $j(document).ready(function(){
         if (window.PIE) {
-            $j('.pie, button.button.black span, button.button span, .dashboard .data-table .link-reorder, .addresses-additional .link-remove, .my-account .link-reorder, .my-account .link-print').each(function() {
+            $j('.pie, button.button.black span, button.button span, .header .form-search .search-autocomplete ul, .dashboard .data-table .link-reorder, .addresses-additional .link-remove, .my-account .link-reorder, .my-account .link-print').each(function() {
                 PIE.attach(this);
             });
         }
