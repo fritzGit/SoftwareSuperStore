3d5e85491ad4acad85fa6964e08eea8370360622
Juan Galvez <j.galvez@pcfritz.de>
2013-04-23 14:34:47 +0200

    css3 for IE < 8

---
+++ skin/frontend/default/softss/js/pie-functions.js
@@ -0,0 +1,9 @@
+(function ($) {
+    $j(document).ready(function(){
+        if (window.PIE) {
+            $j('.pie, button.button.black span, button.button span, .dashboard .data-table .link-reorder, .addresses-additional .link-remove, .my-account .link-reorder, .my-account .link-print').each(function() {
+                PIE.attach(this);
+            });
+        }
+    });
+})(jQuery);
