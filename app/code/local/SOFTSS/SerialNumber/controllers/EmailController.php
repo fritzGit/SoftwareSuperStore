30325db0af1560a11aa4ad7eba9d8112623bb130
Nikolas <n.entwickler@pcfritz.de>
2013-06-03 17:22:37 +0200

    serial number module

---
+++ app/code/local/SOFTSS/SerialNumber/controllers/EmailController.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * Copyright 2013 pcfritz.de Onlinestore GmbH – http://www.pcfritz.de
+ * all rights reserved
+ */
+
+/**
+ * Serial Number account controller
+ *
+ * @encoding    UTF-8
+ * @package     SOFTSS
+ * @subpackage  SOFTSS_SerialNumber
+ * @author      Nikolas Koumarianos <n.entwickler@pcfritz.de>
+ * 
+ */
+class SOFTSS_SerialNumber_EmailController extends Mage_Core_Controller_Front_Action
+{
+      
+
+    public function sendAction()
+    {           
+        
+     
+
+    }
+
+}
+
