53504949b7fb0e7960094a6ecf77b28b9c06b68d
Juan Galvez <j.galvez@pcfritz.de>
2013-06-17 13:27:44 +0200

    product attributes to show in front.

--- app/code/local/SOFTSS/Catalog/sql/softsscatalog_setup/upgrade-0.1.13-0.1.14.php
+++ app/code/local/SOFTSS/Catalog/sql/softsscatalog_setup/upgrade-0.1.13-0.1.14.php
@@ -14,7 +14,6 @@ $installer->updateAttribute('catalog_product','softss_backup_cd_available','is_v
 $installer->updateAttribute('catalog_product','softss_system_requirements','is_visible_on_front',1);
 $installer->updateAttribute('catalog_product','softss_features','is_visible_on_front',1);
 $installer->updateAttribute('catalog_product','softss_multiplayer','is_visible_on_front',1);
-$installer->updateAttribute('catalog_product','softss_marketing_claims','is_visible_on_front',1);
 $installer->updateAttribute('catalog_product','programmversion','is_visible_on_front',1);
 $installer->updateAttribute('catalog_product','publisher','is_visible_on_front',1);
 $installer->updateAttribute('catalog_product','versiontype','is_visible_on_front',1);
